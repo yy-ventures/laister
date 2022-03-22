893e7ec673c35c2d7f794985cb50bf7ccf24139b
Khandaker Nabiul <na.ninad60@gmail.com>
2022-03-22 16:27:59 +0600

    header and footer component created, robot font family added in the project

--- src/components/footer/footer.component.jsx
+++ src/components/footer/footer.component.jsx
@@ -1,11 +1,34 @@
 import React from 'react'
 
+// ICON
+import { FaFacebookF, FaYoutube, FaTwitter, FaTiktok, FaInstagram } from 'react-icons/fa';
+
+// LOGO
+import logo from '../../assets/logo/laister.png'
+
 import './footer.style.scss'
 
 export default function Footer() {
   return (
-    <div>
-        <h2>Footer</h2>
+    <div className='footer'>
+        <div className="footer__left-container">
+          <div className="footer__left-container--logo-container">
+            <img src={logo} alt="laister logo"/>
+          </div>
+          <div className="footer__left-container--text-container">
+            <p>Laister is a small-scale portable digester that converts organic waste to fuel and fertilizer by utilizing household wastes. Laister targets the renewable energy sector with our services and focuses on SDG 4, 7, and 11.</p>
+          </div>
+        </div>
+        <div className="footer__right-container">
+          <div className="footer__right-container--media-container">
+            <a className='link' href="#"><FaFacebookF/></a>
+            <a className='link' href="#"><FaYoutube/></a>
+            <a className='link' href="#"><FaTwitter/></a>
+            <a className='link' href="#"><FaTiktok/></a>
+            <a className='link' href="#"><FaInstagram/></a>
+          </div>
+          <p className='copyright'>Copyright © 2022, Laister</p>
+        </div>
     </div>
   )
 }
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -9,13 +9,13 @@ export default function Header() {
   return (
     <div className='header'>
         <div className="header__logo-container">
-          <img src={logo} alt="laister logo" className="header__logo" />
+          <img src={logo} alt="laister logo"/>
         </div>
         <ul className='header__nav'>
-          <li className='heaeder__nav--item'><a href="#" className='header__nav--link'>Home</a></li>
-          <li className='heaeder__nav--item'><a href="#" className='header__nav--link'>About</a></li>
-          <li className='heaeder__nav--item'><a href="#" className='header__nav--link'>Gallery</a></li>
-          <li className='heaeder__nav--item'><a href="#" className='header__nav--link'>Contact</a></li>
+          <li className='header__nav--item'><a href="#" className='header__nav--link'>Home</a></li>
+          <li className='header__nav--item'><a href="#" className='header__nav--link'>About</a></li>
+          <li className='header__nav--item'><a href="#" className='header__nav--link'>Gallery</a></li>
+          <li className='header__nav--item'><a href="#" className='header__nav--link'>Contact</a></li>
         </ul>
     </div>
   )
