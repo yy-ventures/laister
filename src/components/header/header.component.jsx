3ab14f85aacdab7877bb833956e4e0cd7eedc3ca
Khandaker Nabiul <na.ninad60@gmail.com>
2022-03-27 01:52:51 +0600

    responsive sidebar complete and fixed some issue on responsive layout

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,22 +1,35 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 // IMAGE
 import logo from '../../assets/logo/laister.png';
 
+// ICON
+import { FaBars } from 'react-icons/fa';
+
 import './header.style.scss'
 
 export default function Header() {
+
+  const [active, setActive] = useState(true);
+
+  const handleSidebar = () => {
+    setActive(!active);
+  }
+
+  console.log(active)
+
   return (
     <div className='header'>
-        <div className="header__logo-container">
-          <img src={logo} alt="laister logo"/>
-        </div>
-        <ul className='header__nav'>
-          <li className='header__nav--item'><a href="#" className='header__nav--link'>Home</a></li>
-          <li className='header__nav--item'><a href="#" className='header__nav--link'>About</a></li>
-          <li className='header__nav--item'><a href="#" className='header__nav--link'>Gallery</a></li>
-          <li className='header__nav--item'><a href="#" className='header__nav--link'>Contact</a></li>
-        </ul>
+      <button className='menu-btn' onClick={() => handleSidebar()}><FaBars/></button>
+      <div className="header__logo-container">
+        <img src={logo} alt="laister logo"/>
+      </div>
+      <ul className={active ? 'header__nav active' : 'header__nav'}>
+        <li className='header__nav--item'><a href="#" className='header__nav--link'>Home</a></li>
+        <li className='header__nav--item'><a href="#" className='header__nav--link'>About</a></li>
+        <li className='header__nav--item'><a href="#" className='header__nav--link'>Gallery</a></li>
+        <li className='header__nav--item'><a href="#" className='header__nav--link'>Contact</a></li>
+      </ul>
     </div>
   )
 }
